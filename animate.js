9a665f89589b7b370bc8862c1e39d1149caeff7b
Jenna Hakulinen <jennash@k-d557-mac-32.pk.amk.root>
2021-09-02 11:22:21 +0300

    tallennus tunnilta

---
+++ animate.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function() {
+    $("#fpage").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            let hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().scrollTop
+            }, 800, function(){
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});*/
