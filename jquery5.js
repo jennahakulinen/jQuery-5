3a5c64d5b69a4ccdcd92902d89f95266c0e69d24
jennahakulinen <jenna.hakulinen@metropolia.fi>
2021-09-02 16:34:48 +0300

    html, js, css clean up

---
+++ jquery5.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+// Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+    
+// Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          let hash = this.hash;
+    
+// Using jQuery's animate() method to add smooth page scroll
+// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+$('html, body').animate({
+    scrollLeft: $(hash).offset().left
+    }, 800, function(){
+       
+        // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+        });
+    } // End if
+});
+//Navigation animation
+$('body').mousemove(function(e) {
+    if (e.pageY < 80) {
+        $('nav').slideDown('slow');
+    } else
+        $('nav').slideUp('slow');
+    });
+});
